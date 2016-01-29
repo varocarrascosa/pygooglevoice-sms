caddd98a4e1cdc4e0a3d1f382aa1dc52bbd9b690
William Scanlon <scanlonw1@gmail.com>
2016-01-28 21:00:38 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pygooglevoice-sms',
+      version='0.0.1',
+      description='Python 3 support for Google Voice SMS',
+      url='https://github.com/w1ll1am23/pygooglevoice-sms',
+      author='William Scanlon',
+      license='new BSD',
+      packages=['pygooglevoice-sms'],
+      zip_safe=True)
